93d7edae49affe3e1c2e3e41aa6d99606dc54cee
syawal <syawallllaaa@gmail>
2022-11-08 12:28:05 +0700

    commit keempat

--- praktikum-02-02-array/array_2.php
+++ praktikum-02-02-array/array_2.php
@@ -1,62 +1,74 @@
 <?php
-$programmer_php = array("Andi","Budi","Wati","Gunawan","Baco","Becce","Fatma");
-$programmer_python = array("Andi","Fatma","Fadli","Haris","Baco");
+        $programmer_php = array ("Andi", "Budi", "Wati" , "Gunawan" , "Baco" , "Becce" , "Fatma");
+        $programmer_python = array ("Andi" , "Fatma" , "Fadli" , "Haris" , "Baco");
+        
+        echo "Daftar mahasiswa yang masuk kelompok Programmer PHP dengan nama diurutkan secara Ascending : ";
+        echo "<ul>";
+        sort($programmer_php);
+        foreach ($programmer_php as $data)
+        {
+            echo "<li>".$data."</li>";
+        }
+        echo "</ul>";
 
-//Bagian A
-sort($programmer_php);
-   $countProgrammerPhp = count($programmer_php);
-      echo "Bagian A : <br>";
-      echo "<ul>";
-         for($i=0; $i < $countProgrammerPhp; $i++)
-         {
-            echo "<li>". $programmer_php[$i] . "<br>";
-         }
-      echo "</ul>";
- 
-//Bagian B
-      echo "<br>Bagian B : <br>";
-      echo "<ul>";
-rsort($programmer_python);
-   $countProgrammerPhython = count($programmer_python);
+        echo "Daftar mahasiswa yang masuk kelompok Programmer Python dengan nama diurutkan secara Descending :";
+        echo "<ul>";
+        rsort($programmer_python);
+        foreach ($programmer_python as $data)
+        {
+            echo "<li>".$data."</li>";
+        }
+        echo "</ul>";
 
-         for($x=0; $x < $countProgrammerPhython; $x++)
-         {
-            echo "<li>". $programmer_python[$x] . "<br>";
-         }
-      echo "</ul>";
+        echo "Daftar mahasiswa yang masuk kelompok Programmer PHP tapi tidak termasuk kelompok Programmer Python : ";
+        $bukan_python = array_diff ($programmer_php, $programmer_python);
+        echo "<ul>";
+        foreach ($bukan_python as $data)
+        {
+            echo "<li>".$data."</li>";
+        }
+        echo "</ul>";
 
-//Bagian C
-      echo "<br>Bagian E dan F: <br>";
-      echo "<ul>";
-   $mahasiswa = array_diff($programmer_php,$programmer_python);
-   $countMahasiswa = count($mahasiswa);
 
-         for($m=0; $m < $countMahasiswa; $m++)
-         {
-            echo "<li>" . $mahasiswa[$m] . "<br>";
-         }
- echo "</ul>";
+        echo "Daftar mahasiswa yang masuk kelompok Programmer Python tapi tidak termasuk kelompok Programmer PHP : ";
+        $bukan_php = array_diff ($programmer_python, $programmer_php);
+        echo "<ul>";
+        foreach ($bukan_php as $data)
+        {
+            echo "<li>".$data."</li>";
+        }
+        echo "</ul>";
 
-//Bagian E dan F
-      echo "<br>Bagian E dan F: <br>";
-      echo "<ul>";
-   $mahasiswa = array_merge($programmer_php,$programmer_python);
-   $countMahasiswa = count($mahasiswa);
+        echo "Daftar mahasiswa yang masuk kelompok Programmer PHP dan Programmer Python sekaligus : ";
+        $php_dan_python = array_intersect ($programmer_python, $programmer_php);
+        echo "<ul>";
+        foreach ($php_dan_python as $data)
+        {
+            echo "<li>".$data."</li>";
+        }
+        echo "</ul>";
 
-         for($m=0; $m < $countMahasiswa; $m++)
-         {
-            echo "<li>" . $mahasiswa[$m] . "<br>";
-         }
- echo "</ul>";
 
- //Bagian 2
- array_push($programmer_php, "muhammad syawal");
- echo "<br>Bagian 2 : <br>";
- echo "<ul>";
- $countProgrammerPhp = count($programmer_php);
- for($i=0; $i < $countProgrammerPhp; $i++)
- {
-    echo "<li>".$programmer_php[$i] . "<br>";
- }
- echo "</ul>";
- ?>
+        echo "Daftar seluruh mahasiswa yang masuk kelompok Programmer PHP atau Programmer Python.Satukan terlebih dahulu data dari kedua variabel array dengan teknik merge : ";
+        $semua = array_merge ($programmer_php, $programmer_python);
+        echo "<ul>";
+        sort($semua);
+        foreach ($semua as $data)
+        {
+            echo "<li>".$data."</li>";
+        }
+        echo "</ul>";
+
+        //Bagian 2
+        echo "Lengkapi file array_2 php dengan perintah untuk menambahkan nama Anda ke dalam variabe programmer_php dengan menggunakan perintah array push().";
+        array_push($programmer_php, "Muhammad syawal");
+        echo "<br>Bagian 2 : <br>";
+        echo "<ul>";
+        $countProgrammerPhp = count($programmer_php);
+        for($i=0; $i < $countProgrammerPhp; $i++)
+        {
+            echo "<li>".$programmer_php[$i] . "<br>";
+        }
+        echo "</ul>";
+
+?>
